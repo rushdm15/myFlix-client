66d5be40e407580d68aeb8bf543853e2246c04f4
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-11-13 17:46:44 +0200

    edit, added "morning"

--- src/index.jsx
+++ src/index.jsx
@@ -9,7 +9,7 @@ class MyFlixApplication extends React.Component {
     render() {
         return (
             <div className="my-flix">
-                <div>Good</div>
+                <div>Good morning</div>
             </div>
         );
     }
