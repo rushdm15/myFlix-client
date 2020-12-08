cad967d209ffa42222cc44597d9a35b39bb811dc
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-08 16:21:51 +0200

    import login-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import React, { useReducer } from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
