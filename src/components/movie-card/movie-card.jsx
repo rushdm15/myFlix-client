9c4cef68017fa68ac350bca1d9cc909bc6cb154d
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-08 16:16:21 +0200

    Bootstrap added

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
     render() {
