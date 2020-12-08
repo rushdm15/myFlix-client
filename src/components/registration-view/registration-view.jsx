8803dfe6970c6a5fcac7c6310321990c99cfdd7d
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-08 16:45:02 +0200

    added function components with hooks

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,3 +1,18 @@
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username, password);
+        /* Send a request to the server for authentication */
+        /* then call props.onLoggedIn(username) */
+        props.onLoggedIn(username);
+    };
+
+    return (
         <Form>
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
@@ -18,3 +33,5 @@
                 Submit
   </Button>
         </Form>
+    );
+}
