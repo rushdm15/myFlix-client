a58799cbd8d8adf62e10aebbb2e2a9e61f9c6ab5
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-11 17:21:06 +0200

    axious, post request

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -8,8 +9,17 @@ export function LoginView(props) {
         e.preventDefault();
         console.log(username, password);
         /* Send a request to the server for authentication */
-        /* then call props.onLoggedIn(username) */
-        props.onLoggedIn(username);
+        axios.post('https://flexnet91.herokuapp.com/login', {
+            Username: username,
+            Password: password
+        })
+            .then(response => {
+                const data = response.data;
+                props.onLoggedIn(data);
+            })
+            .catch(e => {
+                console.log('no such user')
+            });
     };
 
     return (
