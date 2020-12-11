2ffcdf9869f3b46c53548007978c7a1f6f0ae141
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-11 17:16:54 +0200

    added column responsive grid

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -11,20 +11,16 @@ export class MovieCard extends React.Component {
         const { movie, onClick } = this.props;
 
         return (
-            <Container>
-                <Row>
-                    <Col>
-                        <Card style={{ width: '16rem' }}>
-                            <Card.Img variant="top" src={movie.ImagePath} />
-                            <Card.Body>
-                                <Card.Title>{movie.Title}</Card.Title>
-                                <Card.Text>{movie.Description}</Card.Text>
-                                <Button onClick={() => onClick(movie)} variant="link">Open</Button>
-                            </Card.Body>
-                        </Card>;
-                    </Col>
-                </Row>
-            </Container>
+            <Col md="3">
+                <Card style={{ width: '16rem' }}>
+                    <Card.Img variant="top" src={movie.ImagePath} />
+                    <Card.Body>
+                        <Card.Title>{movie.Title}</Card.Title>
+                        <Card.Text>{movie.Description}</Card.Text>
+                        <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+                    </Card.Body>
+                </Card>;
+            </Col>
         );
     }
 }
