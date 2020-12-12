d7bc17afcbab9dc411f6d62c58bfd77dd06934d3
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-12 08:44:49 +0200

    axious.post registration

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -12,6 +12,21 @@ export function RegistrationView(props) {
         props.onLoggedIn(username);
     };
 
+    axios.post('https://flexnet91.herokuapp.com/users', {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+    })
+        .then(response => {
+            const data = response.data;
+            console.log(data);
+            window.open('/', '_self'); // the second argument '_self' is necessary so that the page will open in the current tab
+        })
+        .catch(e => {
+            console.log('error registering the user')
+        });
+
     return (
         <Form>
             <Form.Group controlId="formBasicEmail">
