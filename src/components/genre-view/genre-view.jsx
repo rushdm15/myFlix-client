dc1174c2523ea3d773900160bd15808f16c15e7e
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-13 11:06:25 +0200

    Create genre-view

---
+++ src/components/genre-view/genre-view.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export class MovieView extends React.Component {
+
+    constructor() {
+        super();
+
+        this.state = {};
+    }
+
+    render() {
+        const { movie } = this.props;
+        if (!movie) return null;
+
+        return (
+            <div className="movie-view">
+                <img className="movie-poster" src={movie.ImagePath} />
+                {/* <div className="movie-title">
+                    <span className="label">Title: </span>
+                    <span className="value">{movie.Title}</span>
+                </div>
+                <div className="movie-description">
+                    <span className="label">Description: </span>
+                    <span className="value">{movie.Description}</span>
+                </div> */}
+
+                <div className="movie-genre">
+                    <span className="label">Genre: </span>
+                    <span className="value">{movie.Genre.Name}</span>
+                    <Link to={`/genres/${movie.Genre.Name}`}>
+                        <Button variant="link">Genre</Button>
+                    </Link>
+                </div>
+                {/* <div className="movie-director">
+                    <span className="label">Director: </span>
+                    <span className="value">{movie.Director.Name}</span>
+                    <Link to={`/directors/${movie.Director.Name}`}>
+                        <Button variant="link">Director</Button>
+                    </Link>
+                </div> */}
+                <div>
+                    <Button variant="primary" href="/main-view.jsx">Back</Button>
+                </div>
+            </div>
+        );
+    }
+}
+
