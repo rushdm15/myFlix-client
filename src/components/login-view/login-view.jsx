286de4183f91d054ce1d7736729c90541ba8739f
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-13 11:14:44 +0200

    axios.post login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -11,18 +11,20 @@ export function LoginView(props) {
     const handleSubmit = (e) => {
         e.preventDefault();
         /* Send a request to the server for authentication */
-        axios.post('https://flexnet91.herokuapp.com/login', {
-            Username: username,
-            Password: password
-        })
-            .then(response => {
-                const data = response.data;
-                props.onLoggedIn(data);
-            })
-            .catch(e => {
-                console.log('no such user')
-            });
+        props.onLoggedIn(username);
     };
+    axios.post('https://flexnet91.herokuapp.com/login', {
+        Username: username,
+        Password: password
+    })
+        .then(response => {
+            const data = response.data;
+            props.onLoggedIn(data);
+        })
+        .catch(e => {
+            console.log('no such user')
+        });
+};
 
 return (
     <Form>
