bf8839f2a7f37ea83899c28ebba34f9486e89193
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-17 10:46:14 +0200

    removed function moviesApp

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -21,13 +21,6 @@ function movies(state = [], action) {
     }
 }
 
-// function moviesApp(state = {}, action) {
-//     return {
-//         visibilityFilter: visibilityFilter(state.visibilityFilter, action),
-//         movies: movies(state.movies, action)
-//     }
-// }
-
 const moviesApp = combineReducers({
     visibilityFilter,
     movies
