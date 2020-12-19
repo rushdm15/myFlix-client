17a08c37702629303db0f3b6be67a38bbba9df96
rushdm15 <71516868+rushdm15@users.noreply.github.com>
2020-12-19 19:12:20 +0200

    removed devToolsEnhancer

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+// import { devToolsEnhancer } from 'redux-devtools-extension';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MainView from './components/main-view/main-view';
@@ -12,7 +12,8 @@ import moviesApp from './reducers/reducers';
 // Import statement to indicate that you need to bundle './index.scss'
 import './index.scss';
 
-const store = createStore(moviesApp, devToolsEnhancer());
+const store = createStore(moviesApp,);
+// devToolsEnhancer()
 
 // Main component (will eventually use all the others) 
 class MyFlixApplication extends React.Component {
